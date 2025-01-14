d75e00ab67e263dfe0661c71284655ec03b24dc2
Heisenberg <133909724+heisenberg3945@users.noreply.github.com>
2025-01-14 20:29:31 +0100

    New feature: Two button dialog message for easy user choice

--- PS4_Project1/Log.h
+++ PS4_Project1/Log.h
@@ -2,3 +2,4 @@
 #define Displayf(fmt, ...) printf("[DEBUG] %s: " fmt "\n", __FUNCTION__, ##__VA_ARGS__)
 #define Warningf(fmt, ...) printf("[WARNING] %s: " fmt "\n", __FUNCTION__, ##__VA_ARGS__)
 #define Errorf(fmt, ...) printf("[ERROR] %s: " fmt "\n", __FUNCTION__, ##__VA_ARGS__)
+#define Successf(fmt, ...) printf("[SUCCESS] %s: " fmt "\n", __FUNCTION__, ##__VA_ARGS__)
--- PS4_Project1/ebootLoader.cpp
+++ PS4_Project1/ebootLoader.cpp
@@ -2,7 +2,7 @@
 //
 //	PURPOSE: Makes debugging a lot easier by loading an easily replaceable eboot from /data
 //
-//	Build requirements: ORBIS SDK 1.750 or newer and Visual Studio 2017
+//	Build requirements: ORBIS SDK 3.000 or newer and Visual Studio 2017
 
 // headers (most of them aren't even needed or used but whatever)
 #include <scebase.h>
@@ -40,23 +40,83 @@
 #define FALLBACK_PATH "/app0/gtabin.bin"
 #define HANG_TIMEOUT 1000000
 
+int ShowTwoButtonDialog(const char *message, const char *firstbtn, const char *secondbtn) {
+	int32_t ret, status;
+	Displayf("2 button dialog");
+	if (!message || !firstbtn || !secondbtn) {
+		Errorf("Invalid parameters passed, aborting..");
+		return -1;
+	}
+	ret = sceMsgDialogInitialize();
+	if (ret != SCE_OK) {
+		Errorf("Failed to initialize Message Dialog ret %x", ret);
+		return -1;
+	}
+	SceMsgDialogParam dialogParam;
+	sceMsgDialogParamInitialize(&dialogParam);
+
+	SceMsgDialogButtonsParam buttonParams;
+	memset(&buttonParams, 0, sizeof(SceMsgDialogButtonsParam));
+	buttonParams.msg1 = firstbtn;
+	buttonParams.msg2 = secondbtn;
+	
+	SceMsgDialogUserMessageParam messageParam;
+	memset(&messageParam, 0, sizeof(SceMsgDialogUserMessageParam));
+	messageParam.msg = message;
+	messageParam.buttonType = SCE_MSG_DIALOG_BUTTON_TYPE_2BUTTONS;
+	messageParam.buttonsParam = &buttonParams;
+
+	dialogParam.mode = SCE_MSG_DIALOG_MODE_USER_MSG;
+	dialogParam.userMsgParam = &messageParam;
+
+	ret = sceMsgDialogOpen(&dialogParam);
+	if (ret != SCE_OK) {
+		Errorf("Failed to open dialog, ret = %x", ret);
+		return -1;
+	}
+	while (true) {
+		status = sceMsgDialogUpdateStatus();
+		if (status == SCE_COMMON_DIALOG_STATUS_FINISHED) {
+			break;
+		}
+		sceKernelUsleep(1000);
+	}
+	SceMsgDialogResult dialogResult;
+	memset(&dialogResult, 0, sizeof(SceMsgDialogResult));
+	sceMsgDialogGetResult(&dialogResult);
+
+	sceMsgDialogClose();
+	sceMsgDialogTerminate();
+	if (dialogResult.buttonId == SCE_MSG_DIALOG_BUTTON_ID_BUTTON1) {
+		Displayf("Button pressed: %s", firstbtn);
+		return 1;
+	}
+	else if (dialogResult.buttonId == SCE_MSG_DIALOG_BUTTON_ID_BUTTON2) {
+		Displayf("Button pressed: %s", secondbtn);
+		return 0;
+	}
+	Warningf("Invalid selection, will assume %s", firstbtn);
+	return 1;
+	
+}
+
 // Shows a simple dialog message with an ok button
 int ShowDialog(const char* message) {
 	int32_t ret, status;
 	//uint32_t i = 0;
 	Displayf("in ShowDialog");
 	if (!message) {
-		Errorf("Invalid message string passed, aborting to prevent SIGSEGV..");
+		Errorf("Invalid message string passed, aborting..");
 		return -1;
 	}
 	Displayf("message: %s", message);
 	Displayf("initializing messagedialog");
 	ret = sceMsgDialogInitialize();
-	if (ret < SCE_OK) {
+	if (ret != SCE_OK) {
 		Errorf("Failed to initialize Message Dialog ret %x", ret);
 		return ret;
 	}
-	Displayf("Initialized Message Dialog");
+	Successf("Initialized Message Dialog");
 	SceMsgDialogParam dialogParam;
 	sceMsgDialogParamInitialize(&dialogParam);
 
@@ -68,13 +128,12 @@ int ShowDialog(const char* message) {
 	dialogParam.mode = SCE_MSG_DIALOG_MODE_USER_MSG;
 	dialogParam.userMsgParam = &messageParam;
 	//dialogParam.userId = SCE_USER_SERVICE_USER_ID_EVERYONE;
-	Displayf("opening dialog");
+	Displayf("Opening dialog");
 	ret = sceMsgDialogOpen(&dialogParam);
-	if (ret < 0) {
-		Errorf("failed to open dialog, ret = %x", ret);
+	if (ret != SCE_OK) {
+		Errorf("Failed to open dialog, ret = %x", ret);
 		return ret;
 	}
-	
 	Displayf("about to start checking status");
 	while (true) {
 		status = sceMsgDialogUpdateStatus();
@@ -83,7 +142,7 @@ int ShowDialog(const char* message) {
 		}
 		sceKernelUsleep(1000);
 	}
-	Displayf("Closing dialog and returning");
+	Successf("Closing dialog and returning");
 	sceMsgDialogClose();
 	sceMsgDialogTerminate();
 	return SCE_OK;
@@ -93,23 +152,26 @@ int ShowDialog(const char* message) {
 int LoadExecutable(const char *path) {
 	int ret;
 	Displayf("in LoadExecutable");
-	ret = sceKernelCheckReachability(path);
-	const char *execpath = (ret < SCE_OK) ? FALLBACK_PATH : path;
-	if (strcmp(path, execpath)) {
-		Warningf("%s isn't reachable, launching %s", path, execpath); 
-		ShowDialog(static_cast<const char*>("Cannot launch from /data, trying /app0"));
+	ret = ShowTwoButtonDialog("Launch from /data or /app0?", path, FALLBACK_PATH);
+	const char *execpath = (ret == 0 || ret == -1) ? FALLBACK_PATH : path;
+	ret = sceKernelCheckReachability(execpath);
+	if (ret != SCE_OK) {
+		Warningf("Selected path is not reachable, using fallback path");
+		ret = ShowDialog("Selected path is not reachable, using fallback path");
+		execpath = FALLBACK_PATH;
 	}
-	ret = ShowDialog(static_cast<const char*>("Launching Grand Theft Auto V"));
-	if (ret < 0) {
-		Warningf("showdialog failed");
+	
+	ret = ShowDialog("Launching Game eboot");
+	if (ret != SCE_OK) {
+		Errorf("showdialog failed");
 	}
 	ret = sceSystemServiceLoadExec(execpath, NULL);
-	if (ret < SCE_OK) {
+	if (ret != SCE_OK) {
 		Errorf("failed to load executable %s %x", path, ret);
 		return ret;
 	}
 	else {
-		Displayf("successfully loaded executable %s ret %x?", path, ret);
+		Successf("successfully loaded executable %s ret %x?", path, ret);
 		return ret;
 	}
 }
@@ -120,15 +182,15 @@ int main() {
 	Displayf("initing commondialog\n");
 	ret = sceCommonDialogInitialize();
 	if (ret != SCE_OK) {
-		Displayf("Failed to initialize Common Dialog");
+		Errorf("Failed to initialize Common Dialog");
 		return ret;
 	}
 	ret = sceSysmoduleLoadModule(SCE_SYSMODULE_MESSAGE_DIALOG);
 	if (ret < SCE_SYSMODULE_LOADED) {
-		Displayf("Failed to load Message Dialog PRX");
+		Errorf("Failed to load Message Dialog PRX");
 		return ret;
 	}
-	Displayf("loaded message dialog module");
+	Successf("loaded message dialog module");
 	Displayf("Starting %s", TARGET_EXEC_PATH);
 	return LoadExecutable(TARGET_EXEC_PATH);
 }
